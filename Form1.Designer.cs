b58053e731271e483782fe5c8bdfb88f7968dae5
Gurrenm3 <ThomasPMar@hotmail.com>
2019-12-21 09:20:36 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Spin_The_Wheel
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title_Label = new System.Windows.Forms.Label();
+            this.Spin_Button = new System.Windows.Forms.Button();
+            this.Wheel_PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Wheel_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title_Label
+            // 
+            this.Title_Label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Title_Label.AutoSize = true;
+            this.Title_Label.Font = new System.Drawing.Font("MS Gothic", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title_Label.Location = new System.Drawing.Point(31, 24);
+            this.Title_Label.Name = "Title_Label";
+            this.Title_Label.Size = new System.Drawing.Size(524, 48);
+            this.Title_Label.TabIndex = 0;
+            this.Title_Label.Text = "Spin the prize wheel!";
+            // 
+            // Spin_Button
+            // 
+            this.Spin_Button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Spin_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Spin_Button.Location = new System.Drawing.Point(181, 566);
+            this.Spin_Button.Name = "Spin_Button";
+            this.Spin_Button.Size = new System.Drawing.Size(221, 47);
+            this.Spin_Button.TabIndex = 1;
+            this.Spin_Button.Text = "Spin the wheel";
+            this.Spin_Button.UseVisualStyleBackColor = true;
+            this.Spin_Button.Click += new System.EventHandler(this.Spin_Button_Click);
+            // 
+            // Wheel_PictureBox
+            // 
+            this.Wheel_PictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Wheel_PictureBox.BackgroundImage = global::Spin_The_Wheel.Properties.Resources.No_Slots_active;
+            this.Wheel_PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Wheel_PictureBox.Location = new System.Drawing.Point(49, 91);
+            this.Wheel_PictureBox.Name = "Wheel_PictureBox";
+            this.Wheel_PictureBox.Size = new System.Drawing.Size(486, 469);
+            this.Wheel_PictureBox.TabIndex = 2;
+            this.Wheel_PictureBox.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 625);
+            this.Controls.Add(this.Wheel_PictureBox);
+            this.Controls.Add(this.Spin_Button);
+            this.Controls.Add(this.Title_Label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Spin The Wheel!";
+            ((System.ComponentModel.ISupportInitialize)(this.Wheel_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title_Label;
+        private System.Windows.Forms.Button Spin_Button;
+        private System.Windows.Forms.PictureBox Wheel_PictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spin_The_Wheel
+{
+    public partial class Main : Form
+    {
+        string livePath = Environment.CurrentDirectory;
+        
+        Image[] images = new Image [8];
+        public int i = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeWheel_Image()
+        {
+            string resourcePath = livePath + "\\resources\\";
+            
+            images[0] = Image.FromFile(resourcePath + "Slot 1 active.png");
+            images[1] = Image.FromFile(resourcePath + "Slot 2 active.png");
+            images[2] = Image.FromFile(resourcePath + "Slot 3 active.png");
+            images[3] = Image.FromFile(resourcePath + "Slot 4 active.png");
+            images[4] = Image.FromFile(resourcePath + "Slot 5 active.png");
+            images[5] = Image.FromFile(resourcePath + "Slot 6 active.png");
+            images[6] = Image.FromFile(resourcePath + "Slot 7 active.png");
+            images[7] = Image.FromFile(resourcePath + "Slot 8 active.png");
+
+            if (i + 1 > images.Length)
+                i = 0;
+
+            Wheel_PictureBox.Invoke(new Action(() => Wheel_PictureBox.BackgroundImage = images[i]));
+            i++;
+        }
+
+        private void SpinTheWheel()
+        {
+            Random rand = new Random();
+            int fastSpin = rand.Next(45, 60);
+            int medSpin = rand.Next(14, 24);
+            int slowSpin = rand.Next(15, 23);
+
+            //Fast spin speed
+            for (int x = 0; x < fastSpin; x++)
+            {
+                ChangeWheel_Image();
+                GameTick(10);
+            }
+
+            //Meduim spin speed
+            for (int x = 0; x < medSpin; x++)
+            {
+                ChangeWheel_Image();
+                GameTick(40);
+            }
+            
+            //Slow spin speed
+            for (int x = 0; x < slowSpin; x++)
+            {
+                ChangeWheel_Image();
+                GameTick(100);
+            }
+
+            MessageBox.Show("Congrats!! You won prize " + GivePrize());
+        }
+
+        private void Spin_Button_Click(object sender, EventArgs e)
+        {
+            Thread spinWheel = new Thread(SpinTheWheel);
+            spinWheel.Start();
+        }
+
+        private int GivePrize()
+        {
+            int prizeNum = i;
+            return prizeNum;
+        }
+
+        private void GameTick(int tickSpeed)
+        {
+            Thread.Sleep(tickSpeed);
+        }
+    }
+}
